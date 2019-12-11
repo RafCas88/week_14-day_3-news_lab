2759259a50051801d1ce7bb2025336ac157d0a6f
Raffaele Castaldo <raffaelecastaldo88@libero.it>
2019-12-11 11:20:47 +0000

    newslist worked through

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,11 +1,19 @@
 import React from "react"
 import NewsItem from "./NewsItem.js"
 const NewsList = (props) =>{
+
+  const story = props.stories.map((story, index) => {
+    return (<NewsItem key={index} story={fetch("https://hacker-news.firebaseio.com/v0/item/"+story+".json")
+    .then(res => res.json())
+    .then(data => data)}/>
+  )})
+
   return(
     <div>
     <h2> This is a NewsList!</h2>
-    <NewsItem/>
-  </div>)
-}
+    {story}
+    </div>
+  )
+  }
 
-export default NewsList
+  export default NewsList
--- src/container/NewsBox.js
+++ src/container/NewsBox.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react"
 import NewsList from "../components/NewsList.js"
 class NewsBox extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      stories:[]
+    }
+  }
+
+  componentDidMount(){
+    fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
+    .then(res => res.json())
+    .then(data => this.setState({stories: data}))
+  }
+
   render(){
     return(
           <div>
               <h1>This is a news box!</h1>
-              <NewsList/>
+              <NewsList stories={this.state.stories}/>
           </div>
         )
   }
